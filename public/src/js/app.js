943705c085903f5c2f7040cf7c47a93141bec04c
Lizeth <Lizethgio57@gmail.com>
2020-10-07 09:34:18 -0500

    Fetch example

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -35,6 +35,18 @@ var promise = new Promise(function(resolve, reject) {
 //   console.log(newText);
 // });
 
+fetch('https://httpbin.org/ip').
+then(function(response){
+   console.log(response);
+   return response.json();
+})
+ .then(function(data) {
+    console.log(data);
+ })
+ .catch(function(err) {
+  console.log(err);
+  });
+
 promise.then(function(text) {
   return text;
 }).then(function(newText) {
