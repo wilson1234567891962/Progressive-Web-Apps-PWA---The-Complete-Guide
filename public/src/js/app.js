826520d235223775623c77416e75c6850846b154
Lizeth <Lizethgio57@gmail.com>
2020-10-07 09:14:06 -0500

    Fix event

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,10 +1,14 @@
+
 var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then(function() {
-        console.log('Service worker registered!');
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
@@ -14,3 +18,29 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    //resolve('This is executed once the timer is done!');
+    reject({code: 500, message: 'An error occurred!'});
+    //console.log('This is executed once the timer is done!');
+  }, 3000);
+});
+
+// promise.then(function(text) {
+//   return text;
+// }, function(err) {
+//   console.log(err.code, err.message)
+// }).then(function(newText) {
+//   console.log(newText);
+// });
+
+promise.then(function(text) {
+  return text;
+}).then(function(newText) {
+  console.log(newText);
+}).catch(function(err) {
+  console.log(err.code, err.message);
+});
+
+console.log('This is executed right after setTimeout()');
