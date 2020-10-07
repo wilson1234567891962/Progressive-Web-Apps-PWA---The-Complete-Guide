291106e496c4c0663fa51c0562699c697184bb57
Lizeth <Lizethgio57@gmail.com>
2020-10-07 18:42:21 -0500

    Remove the cache

--- public/sw.js
+++ public/sw.js
@@ -26,6 +26,17 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+     caches.keys()
+     .then(function(keyList){
+       return Promise.all(keyList.map( function(key){
+          if(key !== 'static-v2' && key !== 'dynamic') {
+               console.log('[Service worker] Removing old cache', key);
+               return caches.delete(key);
+          }
+       }));
+     })
+  ); 
   return self.clients.claim();
 });
 
