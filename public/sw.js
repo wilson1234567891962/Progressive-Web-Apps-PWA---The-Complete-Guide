a3246cdaadb961d2cc70e7150559f7f8832f94d2
Lizeth <Lizethgio57@gmail.com>
2020-10-07 17:12:34 -0500

    Change the code to cache files

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,20 @@
+
 self.addEventListener('install', function(event) {
-   console.log('[Service Worker] Installing Service Worker ...', event);
-   event.waitUntil(
-       caches.open('static')
-       .then(function(cache) {
-           console.log('[Services worker] Pecaching App Shell');
-           cache.add('src/js/app.js');
-       })
-    );
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.add('/src/js/app.js')
+      })
+  )
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-     return self.clients.claim(); 
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ...', event);
-    event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request));
 });
- 
- 
- 
