cb4cc1b79a057ea2520ed43fd3c8d74db9ddf376
Lizeth <Lizethgio57@gmail.com>
2020-10-09 14:51:17 -0500

    Strategy: cache then network

--- public/sw.js
+++ public/sw.js
@@ -42,7 +42,6 @@ self.addEventListener('activate', function (event) {
   );
   return self.clients.claim();
 });
-
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
 //     caches.match(event.request)
@@ -69,16 +68,32 @@ self.addEventListener('activate', function (event) {
 //   );
 // });
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(res) {
+        return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+      })
+      .catch(function(err) {
+        return caches.match(event.request);
+      })
+  );
+});
+
 // Cache-only
-//self.addEventListener('fetch', function (event) {
-//  event.respondWith(
-//    caches.match(event.request)
-//  );
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//   );
 // });
 
 // Network-only
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    fetch(event.request)
-  );
-});
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     fetch(event.request)
+//   );
+// });
