55f458753e0b8ea2352e629c791a50b8c60d06b4
Lizeth <Lizethgio57@gmail.com>
2020-10-12 11:36:07 -0500

    Displaying notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -24,13 +24,20 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
+function displayConfirmNotification() {
+  var options = {
+    body: 'You successfully subscribed to our Notification service!'
+  };
+  new Notification('Successfully subscribed!', options);
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
     console.log('User Choice', result);
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-
+      displayConfirmNotification();
     }
   });
 }
