95dc1625b70ab5b8a45d986afc40f92f4612880b
Lizeth <Lizethgio57@gmail.com>
2020-10-14 10:39:56 -0500

    Creating our own polyfills

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -12,7 +12,23 @@ var imagePicker = document.querySelector('#image-picker');
 var imagePickerArea = document.querySelector('#pick-image');
 
 function initializeMedia() {
+  if (!('mediaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
 
+  if (!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented!'));
+      }
+
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    }
+  }
 }
 
 function openCreatePostModal() {
