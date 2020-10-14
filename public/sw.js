6d46e329bcfe603aa964bed43366996caa53ab96
Lizeth <Lizethgio57@gmail.com>
2020-10-14 10:59:45 -0500

    Installing workbox and using It

--- public/sw.js
+++ public/sw.js
@@ -63,14 +63,12 @@ self.addEventListener('activate', function (event) {
 });
 
 function isInArray(string, array) {
-  var cachePath;
-  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-    console.log('matched ', string);
-    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
-  } else {
-    cachePath = string; // store the full request (for CDNs)
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === string) {
+      return true;
+    }
   }
-  return array.indexOf(cachePath) > -1;
+  return false;
 }
 
 self.addEventListener('fetch', function (event) {
